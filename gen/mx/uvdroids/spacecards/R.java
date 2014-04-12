acb401918e54d46c49ed326a55c9faa63078aa33
Daniel García Alvarado <danigarciaalva@hotmail.com>
2014-04-11 23:51:44 -0500

    Video background added

--- gen/mx/uvdroids/spacecards/R.java
+++ gen/mx/uvdroids/spacecards/R.java
@@ -9,7 +9,7 @@ package mx.uvdroids.spacecards;
 
 public final class R {
     public static final class array {
-        public static final int tutorial_texts=0x7f0b0000;
+        public static final int tutorial_texts=0x7f0c0000;
     }
     public static final class attr {
         /** <p>Must be a boolean value, either "<code>true</code>" or "<code>false</code>".
@@ -300,32 +300,32 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int vpiUnderlinePageIndicatorStyle=0x7f010005;
     }
     public static final class bool {
-        public static final int default_circle_indicator_centered=0x7f060000;
-        public static final int default_circle_indicator_snap=0x7f060001;
-        public static final int default_line_indicator_centered=0x7f060002;
-        public static final int default_title_indicator_selected_bold=0x7f060003;
-        public static final int default_underline_indicator_fades=0x7f060004;
+        public static final int default_circle_indicator_centered=0x7f070000;
+        public static final int default_circle_indicator_snap=0x7f070001;
+        public static final int default_line_indicator_centered=0x7f070002;
+        public static final int default_title_indicator_selected_bold=0x7f070003;
+        public static final int default_underline_indicator_fades=0x7f070004;
     }
     public static final class color {
-        public static final int default_circle_indicator_fill_color=0x7f050008;
-        public static final int default_circle_indicator_page_color=0x7f050009;
-        public static final int default_circle_indicator_stroke_color=0x7f05000a;
-        public static final int default_line_indicator_selected_color=0x7f05000b;
-        public static final int default_line_indicator_unselected_color=0x7f05000c;
-        public static final int default_title_indicator_footer_color=0x7f05000d;
-        public static final int default_title_indicator_selected_color=0x7f05000e;
-        public static final int default_title_indicator_text_color=0x7f05000f;
-        public static final int default_underline_indicator_selected_color=0x7f050010;
-        public static final int vpi__background_holo_dark=0x7f050000;
-        public static final int vpi__background_holo_light=0x7f050001;
-        public static final int vpi__bright_foreground_disabled_holo_dark=0x7f050004;
-        public static final int vpi__bright_foreground_disabled_holo_light=0x7f050005;
-        public static final int vpi__bright_foreground_holo_dark=0x7f050002;
-        public static final int vpi__bright_foreground_holo_light=0x7f050003;
-        public static final int vpi__bright_foreground_inverse_holo_dark=0x7f050006;
-        public static final int vpi__bright_foreground_inverse_holo_light=0x7f050007;
-        public static final int vpi__dark_theme=0x7f050011;
-        public static final int vpi__light_theme=0x7f050012;
+        public static final int default_circle_indicator_fill_color=0x7f060008;
+        public static final int default_circle_indicator_page_color=0x7f060009;
+        public static final int default_circle_indicator_stroke_color=0x7f06000a;
+        public static final int default_line_indicator_selected_color=0x7f06000b;
+        public static final int default_line_indicator_unselected_color=0x7f06000c;
+        public static final int default_title_indicator_footer_color=0x7f06000d;
+        public static final int default_title_indicator_selected_color=0x7f06000e;
+        public static final int default_title_indicator_text_color=0x7f06000f;
+        public static final int default_underline_indicator_selected_color=0x7f060010;
+        public static final int vpi__background_holo_dark=0x7f060000;
+        public static final int vpi__background_holo_light=0x7f060001;
+        public static final int vpi__bright_foreground_disabled_holo_dark=0x7f060004;
+        public static final int vpi__bright_foreground_disabled_holo_light=0x7f060005;
+        public static final int vpi__bright_foreground_holo_dark=0x7f060002;
+        public static final int vpi__bright_foreground_holo_light=0x7f060003;
+        public static final int vpi__bright_foreground_inverse_holo_dark=0x7f060006;
+        public static final int vpi__bright_foreground_inverse_holo_light=0x7f060007;
+        public static final int vpi__dark_theme=0x7f060011;
+        public static final int vpi__light_theme=0x7f060012;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -334,21 +334,21 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f08000d;
-        public static final int activity_vertical_margin=0x7f08000e;
-        public static final int default_circle_indicator_radius=0x7f080000;
-        public static final int default_circle_indicator_stroke_width=0x7f080001;
-        public static final int default_line_indicator_gap_width=0x7f080003;
-        public static final int default_line_indicator_line_width=0x7f080002;
-        public static final int default_line_indicator_stroke_width=0x7f080004;
-        public static final int default_title_indicator_clip_padding=0x7f080005;
-        public static final int default_title_indicator_footer_indicator_height=0x7f080007;
-        public static final int default_title_indicator_footer_indicator_underline_padding=0x7f080008;
-        public static final int default_title_indicator_footer_line_height=0x7f080006;
-        public static final int default_title_indicator_footer_padding=0x7f080009;
-        public static final int default_title_indicator_text_size=0x7f08000a;
-        public static final int default_title_indicator_title_padding=0x7f08000b;
-        public static final int default_title_indicator_top_padding=0x7f08000c;
+        public static final int activity_horizontal_margin=0x7f09000d;
+        public static final int activity_vertical_margin=0x7f09000e;
+        public static final int default_circle_indicator_radius=0x7f090000;
+        public static final int default_circle_indicator_stroke_width=0x7f090001;
+        public static final int default_line_indicator_gap_width=0x7f090003;
+        public static final int default_line_indicator_line_width=0x7f090002;
+        public static final int default_line_indicator_stroke_width=0x7f090004;
+        public static final int default_title_indicator_clip_padding=0x7f090005;
+        public static final int default_title_indicator_footer_indicator_height=0x7f090007;
+        public static final int default_title_indicator_footer_indicator_underline_padding=0x7f090008;
+        public static final int default_title_indicator_footer_line_height=0x7f090006;
+        public static final int default_title_indicator_footer_padding=0x7f090009;
+        public static final int default_title_indicator_text_size=0x7f09000a;
+        public static final int default_title_indicator_title_padding=0x7f09000b;
+        public static final int default_title_indicator_top_padding=0x7f09000c;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -361,24 +361,25 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int vpi__tab_unselected_pressed_holo=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f04000a;
-        public static final int bottom=0x7f040003;
-        public static final int indicator=0x7f040007;
-        public static final int logo=0x7f040005;
-        public static final int none=0x7f040000;
-        public static final int start_button=0x7f040006;
-        public static final int top=0x7f040004;
-        public static final int triangle=0x7f040001;
-        public static final int tutorial_desc=0x7f040009;
-        public static final int underline=0x7f040002;
-        public static final int viewPager=0x7f040008;
+        public static final int action_settings=0x7f05000b;
+        public static final int bottom=0x7f050003;
+        public static final int indicator=0x7f050008;
+        public static final int logo=0x7f050006;
+        public static final int none=0x7f050000;
+        public static final int start_button=0x7f050007;
+        public static final int top=0x7f050004;
+        public static final int triangle=0x7f050001;
+        public static final int tutorial_desc=0x7f05000a;
+        public static final int tutorial_video=0x7f050005;
+        public static final int underline=0x7f050002;
+        public static final int viewPager=0x7f050009;
     }
     public static final class integer {
-        public static final int default_circle_indicator_orientation=0x7f070000;
-        public static final int default_title_indicator_footer_indicator_style=0x7f070001;
-        public static final int default_title_indicator_line_position=0x7f070002;
-        public static final int default_underline_indicator_fade_delay=0x7f070003;
-        public static final int default_underline_indicator_fade_length=0x7f070004;
+        public static final int default_circle_indicator_orientation=0x7f080000;
+        public static final int default_title_indicator_footer_indicator_style=0x7f080001;
+        public static final int default_title_indicator_line_position=0x7f080002;
+        public static final int default_underline_indicator_fade_delay=0x7f080003;
+        public static final int default_underline_indicator_fade_length=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -387,12 +388,15 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int fragment_tutorial=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int main=0x7f0d0000;
+    }
+    public static final class raw {
+        public static final int space2=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f0a0001;
-        public static final int app_name=0x7f0a0000;
-        public static final int hello_world=0x7f0a0002;
+        public static final int action_settings=0x7f0b0001;
+        public static final int app_name=0x7f0b0000;
+        public static final int hello_world=0x7f0b0002;
     }
     public static final class style {
         /** 
@@ -416,16 +420,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f090005;
+        public static final int AppBaseTheme=0x7f0a0005;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f090006;
-        public static final int TextAppearance_TabPageIndicator=0x7f090003;
-        public static final int Theme_PageIndicatorDefaults=0x7f090000;
-        public static final int Widget=0x7f090001;
-        public static final int Widget_IconPageIndicator=0x7f090004;
-        public static final int Widget_TabPageIndicator=0x7f090002;
+        public static final int AppTheme=0x7f0a0006;
+        public static final int TextAppearance_TabPageIndicator=0x7f0a0003;
+        public static final int Theme_PageIndicatorDefaults=0x7f0a0000;
+        public static final int Widget=0x7f0a0001;
+        public static final int Widget_IconPageIndicator=0x7f0a0004;
+        public static final int Widget_TabPageIndicator=0x7f0a0002;
     }
     public static final class styleable {
         /** Attributes that can be used with a CirclePageIndicator.
--- src/mx/uvdroids/spacecards/WalkthroughActivity.java
+++ src/mx/uvdroids/spacecards/WalkthroughActivity.java
@@ -5,10 +5,15 @@ import mx.uvdroids.spacecards.adapters.ViewAdapter;
 import com.viewpagerindicator.CirclePageIndicator;
 
 import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.view.ViewPager;
 import android.widget.Button;
+import android.widget.VideoView;
 
 
 public class WalkthroughActivity extends Activity {
@@ -16,7 +21,8 @@ public class WalkthroughActivity extends Activity {
 	ViewPager viewPager = null;
 	Button start;
 	CirclePageIndicator mIndicator;
-	Handler handler = null;
+	Handler handler;
+	VideoView video;
 	private Runnable increment = new Runnable() {
 		@Override
 		public void run() {
@@ -26,7 +32,7 @@ public class WalkthroughActivity extends Activity {
 		        viewPager.setCurrentItem((currentItem + 1) % maxItems, true);
 		      else
 		        viewPager.setCurrentItem(0, true);
-		      handler.postDelayed(increment, 2000);
+		      handler.postDelayed(increment, 4000);
 		}
 	};
 	@Override
@@ -34,6 +40,17 @@ public class WalkthroughActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_walktrought);
 		getActionBar().hide();
+		getWindow().setFormat(PixelFormat.TRANSLUCENT);
+		video = (VideoView)findViewById(R.id.tutorial_video);
+		Uri videoUrl = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.space2);
+		video.setVideoURI(videoUrl);
+		video.setOnCompletionListener(new OnCompletionListener() {
+			
+			@Override
+			public void onCompletion(MediaPlayer arg0) {
+				video.start();
+			}
+		});
 		viewPager = (ViewPager)findViewById(R.id.viewPager);
 		viewPager.setAdapter(new ViewAdapter(this));
         mIndicator = (CirclePageIndicator)findViewById(R.id.indicator);
@@ -45,12 +62,14 @@ public class WalkthroughActivity extends Activity {
 	protected void onStart() {
 		super.onStart();
 		handler.removeCallbacks(increment);
-        handler.postDelayed(increment, 2000);
+        handler.postDelayed(increment, 4000);
+        //video.start();
 	}
 	
 	@Override
 	protected void onStop() {
 		super.onStop();
 		handler.removeCallbacks(increment);
+		//video.stopPlayback();
 	}
 }
